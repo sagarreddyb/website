ba64b0858e4ee6240b0ef0b102a95c033c5db5d7
sagarreddyb <79616160+sagarreddyb@users.noreply.github.com>
2021-06-16 11:32:25 +0530

    Add files via upload

---
+++ AdminType.js
@@ -0,0 +1,4 @@
+export const IT_ADMIN = 'IT Admin'
+export const TRIAGE_PROFESSIONAL = 'Triage Professional'
+export const USER_GROUP_ADMIN = 'User Group Admin'
+export const CENTRAL_ADMIN = 'Central Admin'
